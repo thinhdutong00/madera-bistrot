08fc8ed592978503adab8d705ed528aff1949e9f
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-04-09 03:19:22 +0200

    fix

--- app/menu/vini/page.tsx
+++ app/menu/vini/page.tsx
@@ -20,7 +20,7 @@ export default function ViniPage() {
         <div className="flex flex-col justify-between">
           
           {/* BOLLICINE */}
-          <div className="mb-12 lg:mb-16">
+          <div className="mb-12 lg:mb-16 w-full">
             <h3 className="text-2xl font-black uppercase mb-6 italic text-[#642d3a] border-b-2 border-[#642d3a]/10 pb-2">
               Bollicine
             </h3>
@@ -29,18 +29,18 @@ export default function ViniPage() {
               <Image src="/apericena-tradizionale.jpg" alt="Bollicine Madera" fill className="object-cover" />
             </div>
 
-            <div className="space-y-6">
-              {/* Rimosso priceRight per forzare il flex justify-between nativo del componente */}
-              <MenuItemWithDesc label="DORIGATI - Trento Doc Om De Fer" price="7,00€ / 35,00€" desc="Blanc De Blancs 100% Chardonnay" />
-              <MenuItemWithDesc label="LE VEDUTE - Franciacorta Docg Brut" price="35,00€" desc="80% Chardonnay 20% Pinot Nero" />
-              <MenuItemWithDesc label="BELLAVISTA - Franciacorta Docg Alma Brut" price="50,00€" desc="Grande Cuvée" />
-              <MenuItemWithDesc label="FOLLADOR - Valdobbiadene Prosecco Superiore Nani" price="5,00€ / 25,00€" desc="Millestimato Brut" />
-              <MenuItemWithDesc label="FOLLADOR - Valdobbiadene Prosecco Superiore Torri" price="6,00€ / 26,00€" desc="Extra Dry" />
+            {/* w-full assicura che lo spazio per il justify-between sia massimo */}
+            <div className="space-y-6 w-full">
+              <MenuItemWithDesc label="DORIGATI - Trento Doc Om De Fer" price="7,00€ / 35,00€" desc="Blanc De Blancs 100% Chardonnay" priceRight={true} />
+              <MenuItemWithDesc label="LE VEDUTE - Franciacorta Docg Brut" price="35,00€" desc="80% Chardonnay 20% Pinot Nero" priceRight={true} />
+              <MenuItemWithDesc label="BELLAVISTA - Franciacorta Docg Alma Brut" price="50,00€" desc="Grande Cuvée" priceRight={true} />
+              <MenuItemWithDesc label="FOLLADOR - Valdobbiadene Prosecco Superiore Nani" price="5,00€ / 25,00€" desc="Millestimato Brut" priceRight={true} />
+              <MenuItemWithDesc label="FOLLADOR - Valdobbiadene Prosecco Superiore Torri" price="6,00€ / 26,00€" desc="Extra Dry" priceRight={true} />
             </div>
           </div>
 
           {/* BIANCHI */}
-          <div className="lg:mb-0">
+          <div className="lg:mb-0 w-full">
             <h3 className="text-2xl font-black uppercase mb-6 italic text-[#642d3a] border-b-2 border-[#642d3a]/10 pb-2">
               Vini Bianchi
             </h3>
@@ -49,12 +49,12 @@ export default function ViniPage() {
               <Image src="/sfondobottiglieluce.jpg" alt="Vini Bianchi Madera" fill className="object-cover" />
             </div>
 
-            <div className="space-y-6">
-              <MenuItemWithDesc label="VON BLUMEN - Alto Adige Doc Gewürztraminer" price="6,00€ / 25,00€" desc="502 100% Gewürztraminer" />
-              <MenuItemWithDesc label="ROENO - Chardonnay Valdadige Doc" price="5,00€ / 20,00€" desc="Le Fratte 100% Chardonnay" />
-              <MenuItemWithDesc label="DORIGATI - Trentino Pinot Grigio" price="5,00€ / 22,00€" desc="100% Pinot Grigio" />
-              <MenuItemWithDesc label="RONCO DEI TASSI - Venezia Giulia Igp" price="6,00€ / 28,00€" desc="Ribolla Gialla" />
-              <MenuItemWithDesc label="ROVEGLIA - Lugana Doc Wighel" price="5,00€ / 25,00€" desc="100% Lugana" />
+            <div className="space-y-6 w-full">
+              <MenuItemWithDesc label="VON BLUMEN - Alto Adige Doc Gewürztraminer" price="6,00€ / 25,00€" desc="502 100% Gewürztraminer" priceRight={true} />
+              <MenuItemWithDesc label="ROENO - Chardonnay Valdadige Doc" price="5,00€ / 20,00€" desc="Le Fratte 100% Chardonnay" priceRight={true} />
+              <MenuItemWithDesc label="DORIGATI - Trentino Pinot Grigio" price="5,00€ / 22,00€" desc="100% Pinot Grigio" priceRight={true} />
+              <MenuItemWithDesc label="RONCO DEI TASSI - Venezia Giulia Igp" price="6,00€ / 28,00€" desc="Ribolla Gialla" priceRight={true} />
+              <MenuItemWithDesc label="ROVEGLIA - Lugana Doc Wighel" price="5,00€ / 25,00€" desc="100% Lugana" priceRight={true} />
             </div>
           </div>
         </div>
@@ -62,10 +62,16 @@ export default function ViniPage() {
         {/* COLONNA DESTRA: IMMAGINE & ROSSI */}
         <div className="flex flex-col h-full">
           <div className="hidden lg:block relative flex-grow min-h-[400px] rounded-3xl overflow-hidden shadow-2xl mb-12">
-            <Image src="/sfondobottiglieluce.jpg" alt="Selezione Vini Madera" fill className="object-cover" priority />
+            <Image 
+              src="/sfondobottiglieluce.jpg" 
+              alt="Selezione Vini Madera" 
+              fill 
+              className="object-cover"
+              priority
+            />
           </div>
 
-          <div className="mt-auto">
+          <div className="mt-auto w-full">
             <h3 className="text-2xl font-black uppercase mb-6 italic text-[#642d3a] border-b-2 border-[#642d3a]/10 pb-2">
               Vini Rossi
             </h3>
@@ -74,11 +80,11 @@ export default function ViniPage() {
               <Image src="/apericena-vegetariano.jpg" alt="Vini Rossi Madera" fill className="object-cover" />
             </div>
 
-            <div className="space-y-6">
-              <MenuItemWithDesc label="BALIA DI ZOLA - Sangiovese Superiore Doc" price="5,00€ / 22,00€" desc="Balitore 100% Sangiovese" />
-              <MenuItemWithDesc label="CA' RUGATE - Valpolicella Doc Rio Albo" price="5,00€ / 25,00€" desc="45% Corvina - 40% Corvinone - 15% Rondinella" />
-              <MenuItemWithDesc label="TENUTA LILLIANO - Chianti Classico Docg" price="5,00€ / 22,00€" desc="90% Sangiovese - 5% Colorino - 5% Merlot" />
-              <MenuItemWithDesc label="VON BLUMEN - Alto Adige Lagrein" price="5,00€ / 25,00€" desc="100% Lagrein" />
+            <div className="space-y-6 w-full">
+              <MenuItemWithDesc label="BALIA DI ZOLA - Sangiovese Superiore Doc" price="5,00€ / 22,00€" desc="Balitore 100% Sangiovese" priceRight={true} />
+              <MenuItemWithDesc label="CA' RUGATE - Valpolicella Doc Rio Albo" price="5,00€ / 25,00€" desc="45% Corvina - 40% Corvinone - 15% Rondinella" priceRight={true} />
+              <MenuItemWithDesc label="TENUTA LILLIANO - Chianti Classico Docg" price="5,00€ / 22,00€" desc="90% Sangiovese - 5% Colorino - 5% Merlot" priceRight={true} />
+              <MenuItemWithDesc label="VON BLUMEN - Alto Adige Lagrein" price="5,00€ / 25,00€" desc="100% Lagrein" priceRight={true} />
             </div>
           </div>
         </div>
