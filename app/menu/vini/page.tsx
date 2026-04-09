68204fc47b12b39a9d043e35a9aa311ca7be46c1
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-04-09 03:10:47 +0200

    fix

--- app/menu/vini/page.tsx
+++ app/menu/vini/page.tsx
@@ -18,21 +18,24 @@ export default function ViniPage() {
         
         {/* COLONNA SINISTRA: BOLLICINE & BIANCHI */}
         <div className="flex flex-col justify-between">
+          
           {/* BOLLICINE */}
           <div className="mb-12 lg:mb-16">
             <h3 className="text-2xl font-black uppercase mb-6 italic text-[#642d3a] border-b-2 border-[#642d3a]/10 pb-2">
               Bollicine
             </h3>
+            
+            {/* Immagine Mobile: Dopo Titolo / Prima della lista */}
+            <div className="mb-8 lg:hidden relative h-[250px] rounded-2xl overflow-hidden shadow-lg">
+              <Image src="/apericena-tradizionale.jpg" alt="Bollicine Madera" fill className="object-cover" />
+            </div>
+
             <div className="space-y-6">
               <MenuItemWithDesc label="DORIGATI - Trento Doc Om De Fer" price="7,00€ / 35,00€" desc="Blanc De Blancs 100% Chardonnay" priceRight={true} />
               <MenuItemWithDesc label="LE VEDUTE - Franciacorta Docg Brut" price="35,00€" desc="80% Chardonnay 20% Pinot Nero" priceRight={true} />
               <MenuItemWithDesc label="BELLAVISTA - Franciacorta Docg Alma Brut" price="50,00€" desc="Grande Cuvée" priceRight={true} />
               <MenuItemWithDesc label="FOLLADOR - Valdobbiadene Prosecco Superiore Nani" price="5,00€ / 25,00€" desc="Millestimato Brut" priceRight={true} />
               <MenuItemWithDesc label="FOLLADOR - Valdobbiadene Prosecco Superiore Torri" price="6,00€ / 26,00€" desc="Extra Dry" priceRight={true} />
-            </div>
-            {/* Immagine SOLO Mobile dopo Bollicine */}
-            <div className="mt-8 lg:hidden relative h-[250px] rounded-2xl overflow-hidden shadow-lg">
-              <Image src="/apericena-tradizionale.jpg" alt="Bollicine Madera" fill className="object-cover" />
             </div>
           </div>
 
@@ -41,6 +44,12 @@ export default function ViniPage() {
             <h3 className="text-2xl font-black uppercase mb-6 italic text-[#642d3a] border-b-2 border-[#642d3a]/10 pb-2">
               Vini Bianchi
             </h3>
+
+            {/* Immagine Mobile: Dopo Titolo / Prima della lista */}
+            <div className="mb-8 lg:hidden relative h-[250px] rounded-2xl overflow-hidden shadow-lg">
+              <Image src="/sfondobottiglieluce.jpg" alt="Vini Bianchi Madera" fill className="object-cover" />
+            </div>
+
             <div className="space-y-6">
               <MenuItemWithDesc label="VON BLUMEN - Alto Adige Doc Gewürztraminer" price="6,00€ / 25,00€" desc="502 100% Gewürztraminer" priceRight={true} />
               <MenuItemWithDesc label="ROENO - Chardonnay Valdadige Doc" price="5,00€ / 20,00€" desc="Le Fratte 100% Chardonnay" priceRight={true} />
@@ -48,16 +57,12 @@ export default function ViniPage() {
               <MenuItemWithDesc label="RONCO DEI TASSI - Venezia Giulia Igp" price="6,00€ / 28,00€" desc="Ribolla Gialla" priceRight={true} />
               <MenuItemWithDesc label="ROVEGLIA - Lugana Doc Wighel" price="5,00€ / 25,00€" desc="100% Lugana" priceRight={true} />
             </div>
-            {/* Immagine SOLO Mobile dopo Bianchi */}
-            <div className="mt-8 lg:hidden relative h-[250px] rounded-2xl overflow-hidden shadow-lg">
-              <Image src="/sfondobottiglieluce.jpg" alt="Vini Bianchi Madera" fill className="object-cover" />
-            </div>
           </div>
         </div>
 
-        {/* COLONNA DESTRA: ROSSI & IMMAGINE DESKTOP */}
+        {/* COLONNA DESTRA: IMMAGINE DESKTOP & ROSSI */}
         <div className="flex flex-col h-full">
-          {/* Immagine Desktop (nascosta su mobile perché la mettiamo dopo i rossi) */}
+          {/* Immagine Desktop: Incastrata bene in alto per pareggiare le colonne */}
           <div className="hidden lg:block relative flex-grow min-h-[400px] rounded-3xl overflow-hidden shadow-2xl mb-12">
             <Image 
               src="/sfondobottiglieluce.jpg" 
@@ -72,15 +77,17 @@ export default function ViniPage() {
             <h3 className="text-2xl font-black uppercase mb-6 italic text-[#642d3a] border-b-2 border-[#642d3a]/10 pb-2">
               Vini Rossi
             </h3>
+
+            {/* Immagine Mobile: Dopo Titolo / Prima della lista */}
+            <div className="mb-8 lg:hidden relative h-[250px] rounded-2xl overflow-hidden shadow-lg">
+              <Image src="/apericena-vegetariano.jpg" alt="Vini Rossi Madera" fill className="object-cover" />
+            </div>
+
             <div className="space-y-6">
               <MenuItemWithDesc label="BALIA DI ZOLA - Sangiovese Superiore Doc" price="5,00€ / 22,00€" desc="Balitore 100% Sangiovese" priceRight={true} />
               <MenuItemWithDesc label="CA' RUGATE - Valpolicella Doc Rio Albo" price="5,00€ / 25,00€" desc="45% Corvina - 40% Corvinone - 15% Rondinella" priceRight={true} />
               <MenuItemWithDesc label="TENUTA LILLIANO - Chianti Classico Docg" price="5,00€ / 22,00€" desc="90% Sangiovese - 5% Colorino - 5% Merlot" priceRight={true} />
               <MenuItemWithDesc label="VON BLUMEN - Alto Adige Lagrein" price="5,00€ / 25,00€" desc="100% Lagrein" priceRight={true} />
-            </div>
-            {/* Immagine Mobile dopo i Rossi (come richiesto) */}
-            <div className="mt-8 lg:hidden relative h-[250px] rounded-2xl overflow-hidden shadow-lg">
-              <Image src="/apericena-vegetariano.jpg" alt="Vini Rossi Madera" fill className="object-cover" />
             </div>
           </div>
         </div>
