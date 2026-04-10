036144929aac45b92e50b5a8c83d844bc76cb926
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-04-10 19:52:22 +0200

    fix

--- app/menu/drink/page.tsx
+++ app/menu/drink/page.tsx
@@ -160,7 +160,7 @@ export default function DrinkPage() {
           <h3 className="text-2xl font-black uppercase mb-8 italic text-[#642d3a] border-l-4 border-[#642d3a] pl-4">Gintoneria</h3>
           <div className="block lg:hidden w-full aspect-square relative rounded-3xl overflow-hidden shadow-xl mb-8">
   <Image 
-    src="/nome-tua-immagine.jpg" 
+    src="/ginmadera01.jpg" 
     alt="Descrizione categoria" 
     fill 
     className="object-cover"
@@ -191,7 +191,7 @@ export default function DrinkPage() {
           <h3 className="text-2xl font-black uppercase mb-8 italic text-[#642d3a] border-l-4 border-[#642d3a] pl-4">Iced Tea Family</h3>
           <div className="block lg:hidden w-full aspect-square relative rounded-3xl overflow-hidden shadow-xl mb-8">
   <Image 
-    src="/nome-tua-immagine.jpg" 
+    src="/icedteafamily01.jpg" 
     alt="Descrizione categoria" 
     fill 
     className="object-cover"
